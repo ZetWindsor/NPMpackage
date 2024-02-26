e750f0fc923e5a523690bd22b3d1ad32ecc76f1a
Emilio <lemigliot@gmail.com>
2024-02-26 16:01:46 +0100

    fix

---
+++ esercizioPlanet/dist/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import "express-async-errors";
+import morgan from 'morgan';
+const app = express();
+const port = 3001;
+app.use(morgan("dev"));
+let planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
+app.get("", (req, res) => {
+    res.status(200).json(planets);
+});
+app.get('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find((p) => p.id === Number(id));
+    res.status(200).json(planet);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`);
+});
